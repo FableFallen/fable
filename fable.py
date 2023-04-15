8a6df328f6d2498eae75345d02d908cdff69beef
Fable <71018419+FableFallen@users.noreply.github.com>
2023-04-15 03:53:01 -0400

    Update fable.py

--- fable.py
+++ fable.py
@@ -5,9 +5,8 @@ import openai
 import random
 import datetime
 from discord_slash import SlashCommand, SlashContext
-from discord_slash.utils.manage_commands import create_option
 from discord.ext import commands
-
+from discord_slash.utils.manage_commands import create_option
 
 intents = discord.Intents.default()
 intents.typing = False
