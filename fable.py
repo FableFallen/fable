d66c3e864b7f6e1f7f102253937be9cb891ff8b7
Fable <71018419+FableFallen@users.noreply.github.com>
2023-04-15 04:42:13 -0400

    Update fable.py

--- fable.py
+++ fable.py
@@ -5,11 +5,8 @@ import openai
 import random
 import datetime
 from discord_slash import SlashCommand, SlashContext
-from discord_slash.utils import manage_commands
-from discord.app_commands import SlashCommand
-from discord_slash.model import SlashCommandPermissionType
+from discord.ext import commands
 from discord_slash.utils.manage_commands import create_option
-
 
 intents = discord.Intents.default()
 intents.typing = False
@@ -17,33 +14,52 @@ intents.presences = False
 intents.members = True
 intents.message_content = True
 
-client = discord.Client(intents=intents)
+client = commands.Bot(intents=intents)
 slash = SlashCommand(client, sync_commands=True)
-token = os.environ.get('BOT_TOKEN')
+token=os.environ.get('BOT_TOKEN')
 openai.api_key = os.environ.get('AI_TOKEN')
 selected_channel = None
 
+@client.event
+async def on_ready():
+    current_time = datetime.datetime.utcnow().strftime('%A @ %I:%M %p')
+    print(f'[{current_time}] Bot restarted. Logged in as {client.user}')
+    await slash.sync_all_commands()
 
-@app_commands.command()
-@app_commands.has_permissions(manage_messages=True)
-async def clear(
-    ctx: SlashContext,
-    amount: int
-):
+
+@slash.slash(
+    name="clear",
+    description="Clear a specified number of messages in the current channel.",
+    options=[
+        create_option(
+            name="amount",
+            description="The number of messages to delete.",
+            option_type=4,
+            required=True,
+        )
+    ],
+)
+@commands.has_permissions(manage_messages=True)
+async def clear(ctx: SlashContext, amount: int):
     await ctx.channel.purge(limit=amount + 1)
     response = await ctx.send(f"{amount} messages deleted.")
     await asyncio.sleep(3)
     await response.delete()
 
 
-@app_commands.command()
+@slash.slash(
+    name="cerebralInitiation",
+    description="Activate AI setup in the current channel.",
+)
 async def cerebral_initiation(ctx: SlashContext):
     global selected_channel
     selected_channel = ctx.channel
     await ctx.send(f"AI setup is now active in {selected_channel.mention}. Type any message, and the AI will respond as a normal user.")
 
-
-@app_commands.command()
+@slash.slash(
+    name="WhyisRobertonotonline",
+    description="Generate a story about why Roberto is not online",
+)
 async def why_is_roberto_not_online(ctx: SlashContext):
     prompts = [
         "Generate a story about Roberto getting lost in a forest while hiking.",
@@ -66,17 +82,22 @@ async def why_is_roberto_not_online(ctx: SlashContext):
     # Send the generated story to the Discord channel
     await ctx.send(story)
 
-
-@app_commands.command()
-async def join_vc(
-    ctx: SlashContext,
-    voice_channel_name: str,
-):
-    voice_channel = discord.utils.get(
-        ctx.guild.voice_channels, name=voice_channel_name)
+@slash.slash(
+    name="joinvc",
+    description="Ask everyone to join the specified voice channel",
+    options=[
+        create_option(
+            name="voice_channel_name",
+            description="The name of the voice channel",
+            option_type=3,
+            required=True,
+        ),
+    ],
+)
+async def join_vc(ctx: SlashContext, voice_channel_name: str):
+    voice_channel = discord.utils.get(ctx.guild.voice_channels, name=voice_channel_name)
     if voice_channel:
-        general_channel = discord.utils.get(
-            ctx.guild.text_channels, name="general")
+        general_channel = discord.utils.get(ctx.guild.text_channels, name="general")
         if general_channel:
             await general_channel.send(f"@everyone Join the '{voice_channel_name}' voice channel immediately!")
         else:
@@ -106,19 +127,21 @@ async def on_message(message):
     if message.author == client.user:
         return
 
-    if message.channel == selected_channel:
-        prompt = f'Respond to the following message as an advanced AI: \'{message.content}\''
-    response = openai.Completion.create(
-        engine="text-davinci-002",
-        prompt=prompt,
-        max_tokens=150,
-        n=1,
-        stop=None,
-        temperature=0.5,
-    )
+    if message.content.startswith('/'):
+        await client.process_commands(message)
+    elif message.channel == selected_channel:
+        prompt = f"Respond to the following message as an advanced AI: '{message.content}'"
+        response = openai.Completion.create(
+            engine="text-davinci-002",
+            prompt=prompt,
+            max_tokens=150,
+            n=1,
+            stop=None,
+            temperature=0.5,
+        )
 
-    generated_text = response.choices[0].text.strip()
-    await message.channel.send(f"AI Response: {generated_text}")
+        generated_text = response.choices[0].text.strip()
+        await message.channel.send(f"AI Response: {generated_text}")
 
 
 @client.event
